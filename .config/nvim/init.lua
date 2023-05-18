8dc80ed035863772750399eaf5ab2a6fe3e28dfe
Vlad Lisov <butukay@gmail.com>
2023-05-18 16:54:07 +0300

    refactor nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,31 +1,33 @@
+--     __             ____        __        __
+--    / /_  __  __   / __ )__  __/ /___  __/ /______ ___  __
+--   / __ \/ / / /  / __  / / / / __/ / / / //_/ __ `/ / / /
+--  / /_/ / /_/ /  / /_/ / /_/ / /_/ /_/ / ,< / /_/ / /_/ /
+-- /_.___/\__, /  /_____/\__,_/\__/\__,_/_/|_|\__,_/\__, /
+--       /____/                                    /____/
+--
+-- Github: https://github.com/butukay/dotfiles
+
 require("packer").startup(function(use)
 	use "wbthomason/packer.nvim"
 
 	-- appearence
-	use 'Mofiqul/dracula.nvim'
-	use 'projekt0n/github-nvim-theme'
 	use 'folke/tokyonight.nvim'
 
-	use "ryanoasis/vim-devicons"
+	-- use "ryanoasis/vim-devicons"
 	use 'kyazdani42/nvim-web-devicons'
 
-	-- modules
+	-- gui modules
 	use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
 	use 'kyazdani42/nvim-tree.lua'
-	use 'nvim-lualine/lualine.nvim'
-
+	use {'nvim-lualine/lualine.nvim', config = function() require('lualine').setup({ options = { theme = 'tokyonight' } }) end}
 
 	-- helpers
-	use "terrortylor/nvim-comment"  -- comment many lines
-	use "ntpeters/vim-better-whitespace"
+	use { "terrortylor/nvim-comment", config = function() require('nvim_comment').setup() end }  -- comment many lines
+	use "ntpeters/vim-better-whitespace" -- strip whitespaces
 
-	use { "folke/trouble.nvim", requires = "nvim-tree/nvim-web-devicons" }
-	use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" }
-
-
-	--use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-	--use 'tpope/vim-sleuth' -- detect tabstop and shiftwidth automatically
-
+    -- trouble
+	use { "folke/trouble.nvim", requires = "nvim-tree/nvim-web-devicons", config = function() require("trouble").setup() end }
+	use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim", config = function() require("todo-comments").setup() end }
 
 	-- fuzzy find
 	use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
@@ -35,11 +37,8 @@ require("packer").startup(function(use)
 	use { 'nvim-treesitter/nvim-treesitter', run = function() pcall(require('nvim-treesitter.install').update{ with_sync = true }) end }
 	use { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' }
 
-	-- Autocompletion
-	use {
-    	'hrsh7th/nvim-cmp',
-    	requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-	}
+	-- completion
+	use {'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' } }
 
 	-- LSP
 	use { 'neovim/nvim-lspconfig',
@@ -60,14 +59,8 @@ require("packer").startup(function(use)
 	use {'github/copilot.vim'}
 
     -- Discord RPC
-    use 'andweeb/presence.nvim'
-
-    use 'ThePrimeagen/vim-be-good'
+    use {'andweeb/presence.nvim', config = function() require('presence').setup() end}
 end)
-
-require("presence").setup()
-
-vim.cmd[[set spell spelllang=en_us,ru_ru]]
 
 vim.cmd[[colorscheme tokyonight-moon]]
 vim.o.termguicolors = true
@@ -88,17 +81,16 @@ vim.o.updatetime = 100
 vim.o.splitbelow = true
 vim.o.splitright = true
 
-vim.cmd[[set mouse="a"]]
-vim.cmd[[tnoremap <Esc> <C-\><C-n>]]
+vim.o.mouse = a
 
--- folding
-vim.o.foldmethod='expr'
-vim.o.foldexpr='nvim_treesitter#foldexpr()'
-vim.o.foldenable = false
-vim.o.foldnestmax = 1
+vim.o.spelllang = "en_us,ru_ru"
+vim.o.spell = true
 
 vim.o.foldmethod='marker'
+vim.o.foldenable = true
+vim.o.foldnestmax = 1
 
+-- keybindings {{{1
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<Leader>w", "<C-w>k")
@@ -107,11 +99,6 @@ vim.keymap.set("n", "<Leader>s", "<C-w>j")
 vim.keymap.set("n", "<Leader>d", "<C-w>l")
 
 local keymap_opts = { noremap = true, silent = true }
---
--- vim.keymap.set("n", "<Up>", ":!echo bebra<CR>", keymap_opts)
--- vim.keymap.set("n", "<Down>", ":!echo bebra<CR>", keymap_opts)
--- vim.keymap.set("n", "<Right>", ":!echo bebra<CR>", keymap_opts)
--- vim.keymap.set("n", "<Left>", ":!echo bebra<CR>", keymap_opts)
 
 vim.keymap.set("n", "<Leader>j", ":bprevious<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>k", ":bnext<CR>", { silent = true })
@@ -119,9 +106,11 @@ vim.keymap.set("n", "<Leader>q", ":bprevious<CR>:bdelete #<CR>", { silent = true
 vim.keymap.set("n", "<Leader>y", ":%y<CR>")
 
 vim.keymap.set("n", "<Leader>v", ":vsplit term://bash <CR>", { silent = true })
-vim.keymap.set("n", "<Leader>b", "<C-\\><C-n>", { silent = true })
+vim.keymap.set("n", "<Leader>ts", ":split term://bash <CR>", { silent = true })
+vim.keymap.set("n", "<Leader>tv", ":vsplit term://bash <CR>", { silent = true })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
-vim.keymap.set("n", "<F7>", ":edit ~/.config/nvim/init.lua<CR>", { silent = true })
+vim.keymap.set("n", "<F5>", ":edit ~/.config/nvim/init.lua<CR>", { silent = true })
 
 vim.keymap.set('n', '<Leader>e', ":NvimTreeToggle<CR>", keymap_opts)
 vim.keymap.set('n', '<Leader>f', ":Telescope find_files<CR>", keymap_opts)
@@ -129,9 +118,8 @@ vim.keymap.set('n', '<Leader>f', ":Telescope find_files<CR>", keymap_opts)
 vim.keymap.set('n', '<Leader>-', ":split<CR>", keymap_opts)
 vim.keymap.set('n', '<Leader>|', ":vsplit<CR>", keymap_opts)
 
-
 vim.keymap.set('n', '<Space>,', '<Cmd>BufferPrevious<CR>', keymap_opts)
-vim.keymap.set('n', '<Space>.', '<Cmd>BufferNext<CR>', keymap_opts)
+vim.keymap.set('n', '<Space>.', '<Cmd>BufferNext<CR>', keymap_optts)
 vim.keymap.set('n', '<Space><', '<Cmd>BufferMovePrevious<CR>', keymap_opts)
 vim.keymap.set('n', '<Space>>', '<Cmd>BufferMoveNext<CR>', keymap_opts)
 vim.keymap.set('n', '<Space>1', '<Cmd>BufferGoto 1<CR>', keymap_opts)
@@ -151,7 +139,9 @@ vim.keymap.set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', keymap_opts)
 vim.keymap.set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', keymap_opts)
 vim.keymap.set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', keymaps_opts)
 
-require('lualine').setup()
+-- }}}1
+
+-- nvim-tree {{{1
 require("nvim-tree").setup{
 	filters = {
     	dotfiles = false,
@@ -160,61 +150,41 @@ require("nvim-tree").setup{
 	}
 }
 
-require('nvim_comment').setup()
+-- }}}1
 
-require("trouble").setup()
-require("todo-comments").setup()
-
+-- lsp configs {{{1
 require("mason").setup()
 
 local lsp_flags = {
   debounce_text_changes = 150,
 }
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
   -- vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
-require('lspconfig')['pyright'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
-require('lspconfig')['rust_analyzer'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    -- Server-specific settings...
-    settings = {
-      ["rust-analyzer"] = {}
-    }
-}
+require('lspconfig')['pyright'      ].setup{on_attach = on_attach, flags = lsp_flags}
+require('lspconfig')['clangd'       ].setup{on_attach = on_attach, flags = lsp_flags}
+require('lspconfig')['rust_analyzer'].setup{on_attach = on_attach, flags = lsp_flags}
 
-require('lspconfig')['clangd'].setup{
-    on_attach = on_attach
-}
+-- }}}1
 
--- [[ Configure Telescope ]]
+-- telescope {{{1
 require('telescope').setup {
   defaults = {
 	file_ignore_patterns = { 'node_modules', '__pycache__', 'venv'},
@@ -234,8 +204,16 @@ require('telescope').setup {
 
 pcall(require('telescope').load_extension, 'fzf') -- Enable telescope fzf native, if installed
 
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
 vim.keymap.set('n', '<leader>/', function()
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
@@ -243,20 +221,14 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+-- }}}1
 
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
+-- treesitter {{{1
 require('nvim-treesitter.configs').setup {
-  -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'javascript' },
+  ensure_installed = { 'c', 'cpp', 'python', 'rust', 'lua', 'javascript', 'typescript', 'vim', 'help' },
 
   highlight = { enable = true },
-  indent = { enable = true }, --, disable = { 'python' } },
+  indent = { enable = true }, -- disable = { 'python' }
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -300,20 +272,14 @@ require('nvim-treesitter.configs').setup {
         ['[]'] = '@class.outer',
       },
     },
-    -- swap = {
-    --   enable = true,
-    --   swap_next = {
-    --     ['<leader>a'] = '@parameter.inner',
-    --   },
-    --   swap_previous = {
-    --     ['<leader>A'] = '@parameter.inner',
-    --   },
-    -- },
   },
 }
 
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+-- }}}1
+
+-- completions {{{1
+local cmp = require('cmp')
+local luasnip = require('luasnip')
 
 cmp.setup {
   snippet = {
@@ -330,10 +296,14 @@ cmp.setup {
       select = true,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
+      local copilot_keys = vim.fn['copilot#Accept']()
+
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
+      elseif copilot_keys ~= '' and type(copilot_keys) == 'string' then
+        vim.api.nvim_feedkeys(copilot_keys, 'i', true)
       else
         fallback()
       end
@@ -347,10 +317,28 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
+    ["<C-j>"] = cmp.mapping(function(fallback)
+        cmp.mapping.abort()
+        local copilot_keys = vim.fn["copilot#Accept"]()
+        if copilot_keys ~= "" then
+          vim.api.nvim_feedkeys(copilot_keys, "i", true)
+        else
+          fallback()
+        end
+    end, { "i", "s" }),
   },
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   },
 }
+
+-- }}}1
+
+-- copilot {{{1
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+
+-- }}}1
 
